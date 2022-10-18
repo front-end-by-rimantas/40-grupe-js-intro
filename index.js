181f21b8ee7dfc7a7e3ecd97bf96815ad6d3fd11
Rimantas <belauzas@yahoo.com>
2022-10-18 09:28:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log(123);
+console.log('Labas rytas');
+console.log('Labas vakaras');
+console.log(2 + 2);
+console.log('2 + 2');
+console.log('2 + 2 =', 2 + 2);
+
+
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+/*
+Esmines JS temos:
+- kintamieji - informacijos isiminimas
+- salygos sakiniai - palyginimas/patikrinimas
+- ciklai - pasikartojanciu proceduru kartojimas iki kol reikia
+- funkcijos/klases - pasikartojancios logikos surinkimas i viena vieta
+- event listeners - (front'o) stebejimas kada ir koki veiksma padare partotojas
+*/
