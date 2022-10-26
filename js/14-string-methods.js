c43f7fb07a6ee0e9eff290be05b5f72048dfa855
Rimantas <belauzas@yahoo.com>
2022-10-26 11:15:22 +0300

    Strings

--- js/14-string-methods.js
+++ js/14-string-methods.js
@@ -5,6 +5,28 @@ const str = 'Labas rytas Lietuva';
 const rytasPosition = str.indexOf('rytas');
 console.log(rytasPosition);
 
+const labas = 'Labas labas labas labas rytas!';
+const labasPos1 = labas.indexOf('labas');
+console.log('labasPos', labasPos1);
+const labasPos2 = labas.indexOf('labas', labasPos1 + 1);
+console.log('labasPos', labasPos2);
+const labasPos3 = labas.indexOf('labas', labasPos2 + 1);
+console.log('labasPos', labasPos3);
+const labasPos4 = labas.indexOf('labas', labasPos3 + 1);
+console.log('labasPos', labasPos4);
+
+let strCount = 0;
+let strPos = 0;
+const fullStr = 'Labas labas labas labas rytas!';
+const searchTerm = 'labas';
+
+while (fullStr.indexOf(searchTerm, strPos) >= 0) {
+    strCount++;
+    strPos = fullStr.indexOf(searchTerm, strPos) + 1;
+}
+
+console.log('labasCount', strCount);
+
 const xPosition = str.indexOf('x');
 console.log(xPosition);
 
@@ -31,3 +53,16 @@ console.log(visiIkiPaskutinio2);
 
 const paskutinis = str.slice(-1);
 console.log(paskutinis);
+
+let str1 = 'Labas rytas, Lietuva!';
+str1 = str1.replace('rytas', 'vakaras');
+console.log(str1);
+
+let aaa = 'aaaaaaaa';
+aaa = aaa.replace('a', 'B');
+console.log(aaa);
+
+let ccc = 'cccccccc';
+ccc = ccc.replaceAll('c', 'D');
+console.log(ccc);
+
